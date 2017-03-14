9f6f818f3d35322a4a223a073b560344ac78c707
courtney <coa.mcallister@gmail.com>
2017-03-14 17:56:39 -0400

    downloaded bootstrap and removed the for loop for rows in app.js. adjusted classnames and padding in css.

--- public/js/app.js
+++ public/js/app.js
@@ -8,19 +8,13 @@ console.log(screenHeight);
 //width (px) of one box = screenWidth/columns
 //height (px) of one box = screenHeight/rows
 
-var rows = 20;
-var columns = 25;
-for (var i = 0; i < rows; i++){
-  for(var p = 0; p < columns; p++){
-    var new_col_div = document.createElement("div");
-    new_col_div.className = "redstrip";
-    document.body.appendChild(new_col_div);
-    new_col_div.id = `${i}, ${p}` //tag each box with the id row,column
-  }
-  var new_row_div = document.createElement("div");
-  new_row_div.className = "bluestrip";
-  document.body.appendChild(new_row_div);
-  console.log("This is repeat " + i);
+for(var p = 0; p < 501; p++){
+  var new_col_div = document.createElement("div");
+  new_col_div.className = "col-xs-1";
+  document.body.appendChild(new_col_div);
+  new_col_div.id = `${p}` //tag each box with the id row,column
+
 }
+
 
 //upon page reload, choose x number of random boxes to fill in
